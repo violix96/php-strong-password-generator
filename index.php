648851aa0e58bc0d1bcee3cd813f37a5fdf891e0
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-19 17:49:35 +0200

    [UPD] Including bootstrap via cdn and creating the "Password Generator" layout

--- index.php
+++ index.php
@@ -4,11 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-strong-password-generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Generatore di password</title>
 </head>
 
 <body>
-
+    <div class="container text-center mt-5">
+        <h1>Generatore di password</h1>
+        <form method="GET" action="index.php" class="my-4">
+            <div class="mb-3">
+                <label for="length" class="form-label">Lunghezza della Password :</label>
+                <input type="number" id="length" name="length" class="form-control w-50 mx-auto" min="1" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Genera Password</button>
+        </form>
+    </div>
 </body>
 
 </html>
