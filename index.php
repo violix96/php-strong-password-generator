fcb4c650899869356810bf939a9ec66208d3ac67
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-19 19:39:00 +0200

    [UPD] added function to generate random passwords and milestone 1 completed

--- index.php
+++ index.php
@@ -32,7 +32,26 @@
         // funzione per generare la password 
         function generatePassword($length)
         {
-            return 'password provvisoria generata';
+            // stringa che contiene tutti i caratteri che vogliamo includere nella password
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+<>?';
+
+            //variabile $password come stringa vuota
+            $password = '';
+
+            // calcolo dell'indice massimo valido per accedere alla stringa dei caratteri
+            $maxIndex = strlen($characters) - 1;
+
+            // ciclo per generare la password
+            for ($i = 0; $i < $length; $i++) {
+                // genera un numero casuale tra 0 e l'indice massimo
+                $randomIndex = rand(0, $maxIndex);
+
+                // aggiunta del carattere corrispondente all'indice casuale alla password
+                $password .= $characters[$randomIndex];
+            }
+
+            // return della password generata
+            return $password;
         }
         ?>
 
