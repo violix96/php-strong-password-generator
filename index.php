c1cd88009778f6aeb7b085a1d6fc7b2b9fd2f826
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-19 20:41:33 +0200

    [UPD] creation of the functions.php file where it will now contain the function to generate passwords

--- index.php
+++ index.php
@@ -29,30 +29,8 @@
             echo '<div class="alert alert-success">Password Generata: ' . $password . '</div>';
         }
 
-        // funzione per generare la password 
-        function generatePassword($length)
-        {
-            // stringa che contiene tutti i caratteri che vogliamo includere nella password
-            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+<>?';
 
-            //variabile $password come stringa vuota
-            $password = '';
 
-            // calcolo dell'indice massimo valido per accedere alla stringa dei caratteri
-            $maxIndex = strlen($characters) - 1;
-
-            // ciclo per generare la password
-            for ($i = 0; $i < $length; $i++) {
-                // genera un numero casuale tra 0 e l'indice massimo
-                $randomIndex = rand(0, $maxIndex);
-
-                // aggiunta del carattere corrispondente all'indice casuale alla password
-                $password .= $characters[$randomIndex];
-            }
-
-            // return della password generata
-            return $password;
-        }
         ?>
 
     </div>
